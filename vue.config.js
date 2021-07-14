481d85ea87e9de9ce16932f07f1ee944a22abfbd
Kaylee Cummings <kayleercummings@gmail.com>
2021-07-14 15:00:52 -0500

    fixed build performance warning due to asset size limit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
+  configureWebpack: {
+    performance: {
+      hints: false
+    },
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000
+      }
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
-}
+};
